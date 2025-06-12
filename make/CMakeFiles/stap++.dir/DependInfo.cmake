f218368d0cf3e5d37865ba370ab108d3391f0803
WizHUA <1402203132@qq.com>
2025-06-12 23:05:07 +0800

    finish all

---
+++ make/CMakeFiles/stap++.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/wslcp/STAPpp/src/cpp/Bar.cpp" "CMakeFiles/stap++.dir/cpp/Bar.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/Bar.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/Clock.cpp" "CMakeFiles/stap++.dir/cpp/Clock.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/Clock.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/Domain.cpp" "CMakeFiles/stap++.dir/cpp/Domain.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/Domain.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/ElementGroup.cpp" "CMakeFiles/stap++.dir/cpp/ElementGroup.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/ElementGroup.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/LoadCaseData.cpp" "CMakeFiles/stap++.dir/cpp/LoadCaseData.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/LoadCaseData.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/Material.cpp" "CMakeFiles/stap++.dir/cpp/Material.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/Material.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/Node.cpp" "CMakeFiles/stap++.dir/cpp/Node.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/Node.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/Outputter.cpp" "CMakeFiles/stap++.dir/cpp/Outputter.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/Outputter.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/Solver.cpp" "CMakeFiles/stap++.dir/cpp/Solver.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/Solver.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/T3.cpp" "CMakeFiles/stap++.dir/cpp/T3.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/T3.cpp.obj.d"
+  "D:/wslcp/STAPpp/src/cpp/main.cpp" "CMakeFiles/stap++.dir/cpp/main.cpp.obj" "gcc" "CMakeFiles/stap++.dir/cpp/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ make/CMakeFiles/stap++.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stap++.dir/cpp/Bar.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/Bar.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/Clock.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/Clock.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/Domain.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/Domain.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/ElementGroup.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/ElementGroup.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/LoadCaseData.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/LoadCaseData.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/Material.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/Material.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/Node.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/Node.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/Outputter.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/Outputter.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/Solver.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/Solver.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/T3.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/T3.cpp.obj.d"
+  "CMakeFiles/stap++.dir/cpp/main.cpp.obj"
+  "CMakeFiles/stap++.dir/cpp/main.cpp.obj.d"
+  "libstap++.dll.a"
+  "stap++.exe"
+  "stap++.exe.manifest"
+  "stap++.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stap++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
